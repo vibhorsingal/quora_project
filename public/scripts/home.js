39a6320a647e976ced1ce9bfd034b214eea9b615
vibhorsingal <vibhorsingal154@gmail.com>
2021-06-06 23:53:14 +0530

    improved

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,5 +1,20 @@
 $(()=>{
-    $('#answerButton').click(()=>{
-        $('#answer').removeClass('visually-hidden')
+    $('.answerButton').click((e)=>{
+        // console.log(e.target)
+        const id=e.target.getAttribute('id').split("/")[1]
+        const answerButton=`answerButton/${id}`
+        const answer=`answer/${id}`
+        const button=document.getElementById(answerButton)
+        const card=document.getElementById(answer)
+        console.log(card)
+        card.classList.toggle('visually-hidden')
+        if(button.getAttribute('value')=="0"){
+            
+            button.getAttribute("value","1")
+        }
+        else{
+            card.classList.add('visually-hidden')
+            button.setAttribute("value","0")
+        }
     })
 })
