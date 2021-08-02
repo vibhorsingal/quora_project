1cb1e0939b9f4ef8fdf1cd2bdc2627ceffc992da
vibhorsingal <vibhorsingal154@gmail.com>
2021-08-02 20:39:52 +0530

    redirect url corrected

--- config/passport_google.js
+++ config/passport_google.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt')
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/redirect"
+    callbackURL: process.env.REDIRECT_URL || "http://localhost:5000/auth/google/redirect"
 },
     async (accessToken, refreshToken, profile, done) => {
         const user = await Users.findOne({ email: profile.emails[0].value })
